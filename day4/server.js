8d14d6af9ca424c0ad22aaf873943f386d63e686
Sanchit Gupta <sanc.aot24@gmail.com>
2025-07-23 03:25:16 +0530

    note project

--- day4/server.js
+++ day4/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+app.use(express.json()) // here we have to use this middle ware to show data on postman because express doesnot read directly.
+// here we will create routes to make notes and get data and show it again on client side.
 
+let notes = [];
+app.get('/about', (req,res)=>{
+    res.send(notes).json({
+        
+    })
+})
 
+app.post('/about', (req,res)=>{
+    notes.push(req.body)
+    res.json({
+        message: "note added!"
+    })
+})
 
 
 
